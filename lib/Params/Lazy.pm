119571b83d9e2e90f8238419e8e42a7a681e5b8a
Brian Fraser <fraserbn@gmail.com>
2013-08-09 07:49:40 -0300

    Several doc improvements

--- lib/Params/Lazy.pm
+++ lib/Params/Lazy.pm
@@ -62,17 +62,15 @@ sub import {
 
 1;
 
+=encoding utf8
+
 =head1 NAME
 
-Params::Lazy - The great new Params::Lazy!
+Params::Lazy - Transparent lazy arguments for subroutines.
 
 =head1 VERSION
 
 Version 0.01
-
-=cut
-
-
 
 =head1 SYNOPSIS
 
@@ -105,6 +103,7 @@ Version 0.01
                 push @retvals, $_;
             }
         }
+        return @retvals;
     }
     use Params::Lazy fakegrep => ':@';
     
@@ -141,7 +140,23 @@ Runs the delayed code.
 
 =head1 LIMITATIONS
 
+=over
+
+=item *
+
+When using the C<:> prototype, these two cases are indistinguishable:
+
+    myfunction { ... }
+    myfunction sub { ... }
+
+Which means that C<mymap sub { ... }, 1..10> will work
+differently than the default map.
+
+=item *
+
 Strange things will happen if you goto LABEL out of a lazy argument.
+
+=item *
 
 It's also important to note that delayed arguments are *not* closures,
 so storing them for later use will likely lead to crashes, segfaults,
@@ -150,101 +165,32 @@ and your cat.  Passing them to other functions should work fine, but
 returning them to the place where they were delayed is generally a
 bad idea.
 
-Throwing an exception within a delayed eval might not work properly
-on older Perls (particularly, the 5.8 series).  Similarly, there's
-a bug in Perl 5.10.1 that makes delaying a regular expression likely
-to crash the program.
+=item *
+
+Throwing an exception within a delayed eval might not work
+properly on older Perls (particularly, the 5.8 series).
+Similarly, there's a bug in Perl 5.10.1 that makes delaying
+a regular expression likely to crash the program.
+
+=item *
 
 Finally, delayed arguments, although intended to be faster & more light
 weight than coderefs, are currently about twice as slow as passing
 a coderef and dereferencing it, so beware!
 
-=head1 AUTHOR
-
-Brian Fraser, C<< <fraserbn at gmail.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-params-lazy at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Params-Lazy>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Params::Lazy
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Params-Lazy>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Params-Lazy>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Params-Lazy>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Params-Lazy/>
-
 =back
 
+=head1 AUTHOR, LICENSE AND COPYRIGHT
+
+Copyright 2013 Brian Fraser, C<< <fraserbn at gmail.com> >>
+
+This program is free software; you may redistribute it and/or modify it under the same terms as perl.
 
 =head1 ACKNOWLEDGEMENTS
 
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2013 Brian Fraser.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+To Scala for the inspiration, to #p5p in general for holding my hand as I
+stumbled through the callchecker, and to Zefram for L<Devel::CallChecker>
+and spotting a leak.
 
 =cut
 
